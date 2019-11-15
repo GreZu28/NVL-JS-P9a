7c2e7dbab0de8a06c9c3e67bb133bed513c10998
GreZu28 <57643026+GreZu28@users.noreply.github.com>
2019-11-15 13:15:45 +0100

    Práctica de matrices I

---
+++ matrices-I.js
@@ -0,0 +1,16 @@
+//Crea un array donde cada elemento sea un lista de dos valores.
+var miArray = [[2,4], [5,6], [3,1] [7,9]];
+
+//1. Indica la longitud del array
+var longArray = miArray.length;
+
+//2. Indica la longitud de uno de los elementos del array
+//En este caso he accedido al primer elemento del array a través del índice cero
+var longElemento = miArray[0].length;
+
+//3. Muestra por pantalla el valor del tercer elemento de la lista del array. Y dentro de ese tercer elemento, su segundo elemento.
+var $3erElemArray = miArray[2];
+alert($3erElemArra);
+
+var $2do3erElem = miArray[2][1];
+alert($2do3erElem);
